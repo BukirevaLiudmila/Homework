a93a226dfcc18d1f0f7a18c9e147e7c0e93b6893
Liudmila <226585@niuitmo.ru>
2016-10-15 19:43:01 +0300

    Add hw3

---
+++ Hw3/showJSON.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	$('#show').click(function() {
+		$.ajax ({
+			url: 'file.json',
+			dataType: 'json', 
+			method: 'GET'        
+		})
+		.done(function(res){
+			let data = '<h2>' + res.title + '</h2>';     // создаём переменную data и помещаем в неё значение title из JSON-файла (Movies directed by David Fincher)
+			for (let i = 0; i < res.movies.length; ++i) {
+				// заранее помещаем жанры в переменную genre
+				let genre = '';
+				for (let j = 0; j < res.movies[i].genre.length; ++j) {
+					genre += res.movies[i].genre[j] + (j < res.movies[i].genre.length - 1 ? ', ' : '');
+				}
+				data += '<div> "' + res.movies[i].name + '" <ul><li>'    // добавляем в переменную data название фильма в кавычках
+				+ 'Year of issue: ' + res.movies[i].year + '</li><li>' 
+				+ 'Genre: ' + genre + '</li><li>'                                    // добавляем в переменную data заранее подготовленние жанры
+				+ 'Budget: $' + res.movies[i].budget + '</li></ul></div>';
+			}
+			$('#data').append(data);
+		})
+		.fail(function(e) {
+			console.log('error', e);
+		});
+	});
+});
+
